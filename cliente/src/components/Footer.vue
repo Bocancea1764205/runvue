0b9add9b399a2fcc487fa7f715ec1b005ac5c25b
Bocancea1764205 <bocancea.1764205@studenti.uniroma1.it>
2021-06-02 16:38:45 +0200

    Revert "Revert "Revert "Revert "Revert "Merge branch 'main' of https://github.com/Bocancea1764205/runvue into main"""""
    This reverts commit 59d1ed71366e1c8b9b8a446a8915806085d2297e.

--- cliente/src/components/Footer.vue
+++ cliente/src/components/Footer.vue
@@ -1,23 +1,35 @@
 <template>
-  <v-footer dark padless>
-    <v-card class="flex text-center" flat tile>
+  <v-footer
+    dark
+    padless
+  >
+    <v-card
+      class="flex text-center"
+      flat
+      tile
+    >
       <v-card-title class="grey darken-4">
-        <strong class="subheading"
-          >Connettiti con noi sui social network!</strong
-        >
+        <strong class="subheading">Connettiti con noi sui social network!</strong>
 
         <v-spacer></v-spacer>
 
-        <v-btn v-for="icon in icons" :key="icon" class="mx-4" dark icon>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4"
+          dark
+          icon
+        >
           <v-icon size="24px">
             {{ icon }}
           </v-icon>
         </v-btn>
       </v-card-title>
 
+      <v-divider></v-divider>
+      
       <v-card-text class="grey darken-4 white--text pt-0">
-        vast-retreat-83495.herokuapp.com/ - prodotto da Mihai Dragos Bocancea e
-        Alessio Buccarella - © Tutti i diritti riservati.
+        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
       </v-card-text>
 
       <v-card-text class="deep-orange py-2 white--text text-center">
@@ -30,7 +42,11 @@
 <script>
   export default {
     data: () => ({
-      icons: ["mdi-facebook", "mdi-twitter", "mdi-instagram"],
+      icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-instagram',
+      ],
     }),
-  };
+  }
 </script>
