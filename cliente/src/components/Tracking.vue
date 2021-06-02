6523b947503bc75b6b3b182ab83b12bb52afc6e6
Bocancea1764205 <bocancea.1764205@studenti.uniroma1.it>
2021-06-02 16:19:52 +0200

    Revert "Revert "Revert "Merge branch 'main' of https://github.com/Bocancea1764205/runvue into main"""
    This reverts commit 3b462f9b82c1e775d036354b7647f8d8b855e901.

--- cliente/src/components/Tracking.vue
+++ cliente/src/components/Tracking.vue
@@ -7,31 +7,15 @@
       style="width: 100% !important; height: 500px !important;"
     >
     </v-card>
-    <v-bottom-sheet v-model="sheet" inset>
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn
-          v-if="authenticated && !startedStopwatch && !startedCountdown"
-          v-on:click="
-            saveRun($store.state.start.run);
-            meters = 0;
-          "
-          color="success"
-          dark
-          v-bind="attrs"
-          v-on="on"
-          >Salva corsa</v-btn
-        >
-      </template>
-      <v-sheet class="text-center" height="130px">
-        <v-btn class="mt-6" text color="error" @click="sheet = !sheet"
-          ><v-icon dark left> mdi-close-circle </v-icon>close
-        </v-btn>
-        <div class="my-3">
-          <v-icon>mdi-checkbox-marked-circle-outline</v-icon>
-          Corsa salvata con successo!
-        </div>
-      </v-sheet>
-    </v-bottom-sheet>
+    <v-btn
+      v-if="authenticated && !startedStopwatch && !startedCountdown"
+      v-on:click="
+        saveRun($store.state.start.run);
+        meters = 0;
+      "
+      color="success"
+      >Salva corsa</v-btn
+    >
 
     <v-btn
       v-show="!startedStopwatch && !startedCountdown && meters > 0"
@@ -47,219 +31,212 @@
 </template>
 
 <script>
-  var coordinates = [];
-  let lastTwoCoordinates = [];
-  const geo = {
-    id: undefined,
-  };
-  import { mapGetters, mapActions } from "vuex";
-  import mapboxgl from "mapbox-gl";
-  import Vue from "vue";
-  Vue.use(geo);
-  Vue.use(mapboxgl);
-  export default {
-    name: "Tracking",
-    data: () => ({
-      sheet: false,
-      soglia: "",
-      meters: "",
-      realtime_meters: "",
-      map: "",
+var coordinates = [];
+let lastTwoCoordinates = [];
+const geo = {
+  id: undefined,
+};
+import { mapGetters, mapActions } from "vuex";
+import mapboxgl from "mapbox-gl";
+import Vue from "vue";
+Vue.use(geo);
+Vue.use(mapboxgl);
+export default {
+  name: "Tracking",
+  data: () => ({
+    soglia: "",
+    meters: "",
+    realtime_meters: "",
+    map: "",
+  }),
+  created() {
+    this.soglia = 0;
+    this.meters = 0;
+    this.map = "";
+    this.realtime_meters = "";
+  },
+  watch: {
+    startedStopwatch: function(value) {
+      if (value) {
+        return this.trackPosition();
+      } else {
+        return this.stopTracking();
+      }
+    },
+  },
+  computed: {
+    ...mapGetters({
+      authenticated: "auth/authenticated",
+      user: "auth/user",
     }),
-    created() {
-      this.soglia = 0;
-      this.meters = 0;
-      this.map = "";
-      this.realtime_meters = "";
+    startedStopwatch: function() {
+      return this.$store.state.start.startedStopwatch;
     },
-    watch: {
-      startedStopwatch: function(value) {
-        if (value) {
-          return this.trackPosition();
-        } else {
-          return this.stopTracking();
-        }
-      },
+    startedCountdown: function() {
+      return this.$store.state.start.startedStopwatch;
     },
-    computed: {
-      ...mapGetters({
-        authenticated: "auth/authenticated",
-        user: "auth/user",
-      }),
-      startedStopwatch: function() {
-        return this.$store.state.start.startedStopwatch;
-      },
-      startedCountdown: function() {
-        return this.$store.state.start.startedStopwatch;
-      },
-    },
-    methods: {
-      ...mapActions({
-        reset: "start/reset",
-        setPath: "start/setPath",
-        setMeters: "start/setMeters",
-        setDate: "start/setDate",
-        saveRun: "start/saveRun",
-      }),
-      trackPosition: async function() {
-        if (!navigator.geolocation) {
-          alert("Localizzazione GPS non autorizzata o non supportata!");
-          return;
-        }
-        try {
-          await navigator.wakeLock.request("screen");
-        } catch (err) {
-          console.log(`${err.name}, ${err.message}`);
-          alert(
-            "Questo browser non supporta questa funzionalità. Attualmente Chrome, Edge e Opera sono i browser supportati"
-          );
-          return;
-        }
-        coordinates = [];
-        geo.id = navigator.geolocation.watchPosition(
-          this.successPosition,
-          this.failurePosition,
-          {
-            enableHighAccuracy: true,
-            timeout: Infinity,
-            maximumAge: 5000,
-          }
-        );
-      },
-      successPosition: function(position) {
-        if (
-          (this.soglia += 1) > 5 &&
-          position.coords.accuracy <
-            (process.env.NODE_ENV === "production" ? 20 : Infinity)
-        ) {
-          coordinates.push([
-            position.coords.longitude,
-            position.coords.latitude,
-          ]);
-          lastTwoCoordinates.push([
-            position.coords.longitude,
-            position.coords.latitude,
-          ]);
-          if (lastTwoCoordinates.length > 1) {
-            this.meters += this.realtimeMeters(lastTwoCoordinates);
-            this.realtimeMetersPrint(this.meters);
-            lastTwoCoordinates.shift();
-            console.log(this.meters);
-          }
-          console.log(coordinates);
-          console.log(position.coords.accuracy);
-        }
-      },
-      failurePosition: function(err) {
-        alert(
-          "Codice di errore: " +
-            err.code +
-            "Messaggio di errore: " +
-            err.message
-        );
-        navigator.geolocation.clearWatch(geo.id);
-        coordinates = [];
+  },
+  methods: {
+    ...mapActions({
+      reset: "start/reset",
+      setPath: "start/setPath",
+      setMeters: "start/setMeters",
+      setDate: "start/setDate",
+      saveRun: "start/saveRun",
+    }),
+    trackPosition: async function() {
+      if (!navigator.geolocation) {
+        alert("Localizzazione GPS non autorizzata o non supportata!");
         return;
-      },
-      stopTracking() {
-        navigator.geolocation.clearWatch(geo.id);
-        this.setPath(coordinates);
-        this.setMeters(this.meters);
-        this.setDate(Date.now());
-        console.log(this.$store.state.start.run.path);
-        console.log(this.$store.state.start.run.meters);
-        console.log(this.$store.state.start.run.date);
-        console.log(this.$store.state.start.run);
-
-        this.drawMap();
-      },
-      //********************Mapbox Draw Map******************/
-      drawMap() {
-        const length = coordinates.length;
-        this.soglia = 0;
-        if (length > 1) {
-          mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_API;
-          this.map = new mapboxgl.Map({
-            container: "map",
-            style: "mapbox://styles/mapbox/streets-v11",
-            center: coordinates[Math.floor(coordinates.length / 2)],
-            zoom: 15,
-          });
-          this.map.on("dataloading", () => {
-            window.dispatchEvent(new Event("resize"));
-          });
-          this.map.on("load", () => {
-            this.map.addSource("route", {
-              type: "geojson",
-              data: {
-                type: "Feature",
-                properties: {},
-                geometry: {
-                  type: "LineString",
-                  coordinates: coordinates,
-                },
-              },
-            });
-            this.map.addLayer({
-              id: "route",
-              type: "line",
-              source: "route",
-              layout: {
-                "line-join": "round",
-                "line-cap": "round",
-              },
-              paint: {
-                "line-color": "#f26958",
-                "line-width": 8,
-              },
-            });
-          });
+      }
+      try {
+        await navigator.wakeLock.request("screen");
+      } catch (err) {
+        console.log(`${err.name}, ${err.message}`);
+        alert(
+          "Questo browser non supporta questa funzionalità. Attualmente Chrome, Edge e Opera sono i browser supportati"
+        );
+        return;
+      }
+      coordinates = [];
+      geo.id = navigator.geolocation.watchPosition(
+        this.successPosition,
+        this.failurePosition,
+        {
+          enableHighAccuracy: true,
+          timeout: Infinity,
+          maximumAge: 5000,
         }
-      },
-      //**********************Realtime Meters*******************/
-      realtimeMeters(lastTwoCoordinates) {
-        if (
-          lastTwoCoordinates[0][0] !== lastTwoCoordinates[1][0] &&
-          lastTwoCoordinates[0][1] !== lastTwoCoordinates[1][1]
-        ) {
-          let puntoA = new mapboxgl.LngLat(
-            lastTwoCoordinates[0][0],
-            lastTwoCoordinates[0][1]
-          );
-          let puntoB = new mapboxgl.LngLat(
-            lastTwoCoordinates[1][0],
-            lastTwoCoordinates[1][1]
-          );
-          return puntoA.distanceTo(puntoB);
-        }
-        return 0;
-      },
-      realtimeMetersPrint(meters) {
-        let realtime_meters = Math.floor(meters);
-        if (realtime_meters >= 1000) {
-          this.realtime_meters = `${(realtime_meters / 1000).toFixed(3)} km`;
-        } else if (realtime_meters > 1) {
-          this.realtime_meters = `${realtime_meters} metri`;
-        } else if (realtime_meters === 1) {
-          this.realtime_meters = `1 metro`;
-        } else {
-          this.realtime_meters = `0 metri`;
-        }
-      },
+      );
     },
-  };
+    successPosition: function(position) {
+      if (
+        (this.soglia += 1) > 5 &&
+        position.coords.accuracy <
+          (process.env.NODE_ENV === "production" ? 20 : Infinity)
+      ) {
+        coordinates.push([position.coords.longitude, position.coords.latitude]);
+        lastTwoCoordinates.push([
+          position.coords.longitude,
+          position.coords.latitude,
+        ]);
+        if (lastTwoCoordinates.length > 1) {
+          this.meters += this.realtimeMeters(lastTwoCoordinates);
+          this.realtimeMetersPrint(this.meters);
+          lastTwoCoordinates.shift();
+          console.log(this.meters);
+        }
+        console.log(coordinates);
+        console.log(position.coords.accuracy);
+      }
+    },
+    failurePosition: function(err) {
+      alert(
+        "Codice di errore: " + err.code + "Messaggio di errore: " + err.message
+      );
+      navigator.geolocation.clearWatch(geo.id);
+      coordinates = [];
+      return;
+    },
+    stopTracking() {
+      navigator.geolocation.clearWatch(geo.id);
+      this.setPath(coordinates);
+      this.setMeters(this.meters);
+      this.setDate(Date.now());
+      console.log(this.$store.state.start.run.path);
+      console.log(this.$store.state.start.run.meters);
+      console.log(this.$store.state.start.run.date);
+      console.log(this.$store.state.start.run);
+
+      this.drawMap();
+    },
+    //********************Mapbox Draw Map******************/
+    drawMap() {
+      const length = coordinates.length;
+      this.soglia = 0;
+      if (length > 1) {
+        mapboxgl.accessToken = process.env.VUE_APP_MAPBOX_API;
+        this.map = new mapboxgl.Map({
+          container: "map",
+          style: "mapbox://styles/mapbox/streets-v11",
+          center: coordinates[Math.floor(coordinates.length / 2)],
+          zoom: 15,
+        });
+        this.map.on("dataloading", () => {
+          window.dispatchEvent(new Event("resize"));
+        });
+        this.map.on("load", () => {
+          this.map.addSource("route", {
+            type: "geojson",
+            data: {
+              type: "Feature",
+              properties: {},
+              geometry: {
+                type: "LineString",
+                coordinates: coordinates,
+              },
+            },
+          });
+          this.map.addLayer({
+            id: "route",
+            type: "line",
+            source: "route",
+            layout: {
+              "line-join": "round",
+              "line-cap": "round",
+            },
+            paint: {
+              "line-color": "#f26958",
+              "line-width": 8,
+            },
+          });
+        });
+      }
+    },
+    //**********************Realtime Meters*******************/
+    realtimeMeters(lastTwoCoordinates) {
+      if (
+        lastTwoCoordinates[0][0] !== lastTwoCoordinates[1][0] &&
+        lastTwoCoordinates[0][1] !== lastTwoCoordinates[1][1]
+      ) {
+        let puntoA = new mapboxgl.LngLat(
+          lastTwoCoordinates[0][0],
+          lastTwoCoordinates[0][1]
+        );
+        let puntoB = new mapboxgl.LngLat(
+          lastTwoCoordinates[1][0],
+          lastTwoCoordinates[1][1]
+        );
+        return puntoA.distanceTo(puntoB);
+      }
+      return 0;
+    },
+    realtimeMetersPrint(meters) {
+      let realtime_meters = Math.floor(meters);
+      if (realtime_meters >= 1000) {
+        this.realtime_meters = `${(realtime_meters / 1000).toFixed(3)} km`;
+      } else if (realtime_meters > 1) {
+        this.realtime_meters = `${realtime_meters} metri`;
+      } else if (realtime_meters === 1) {
+        this.realtime_meters = `1 metro`;
+      } else {
+        this.realtime_meters = `0 metri`;
+      }
+    },
+  },
+};
 </script>
 
 <style lang="css">
-  @import "https://api.mapbox.com/mapbox-gl-js/v2.2.0/mapbox-gl.css";
-  *:focus {
-    outline: none;
-  }
-  .mapboxgl-ctrl-attrib-button {
-    display: none !important;
-  }
-  .mapboxgl-ctrl-bottom-left,
-  .mapboxgl-ctrl-bottom-right {
-    display: none !important;
-  }
+@import "https://api.mapbox.com/mapbox-gl-js/v2.2.0/mapbox-gl.css";
+*:focus {
+  outline: none;
+}
+.mapboxgl-ctrl-attrib-button {
+  display: none !important;
+}
+.mapboxgl-ctrl-bottom-left,
+.mapboxgl-ctrl-bottom-right {
+  display: none !important;
+}
 </style>
