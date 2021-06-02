fc236827a01fdeba2efedea48d81c54e528d73be
Bocancea1764205 <bocancea.1764205@studenti.uniroma1.it>
2021-06-02 16:37:11 +0200

    Revert "Revert "Revert "Revert "Revert "Merge branch 'main' of https://github.com/Bocancea1764205/runvue into main"""""
    This reverts commit 59d1ed71366e1c8b9b8a446a8915806085d2297e.

--- cliente/src/views/About.vue
+++ cliente/src/views/About.vue
@@ -1,11 +1,8 @@
 <template>
-  <v-app>
-    <h1 class="text-center" color="deep-orange">About</h1>
-    <v-divider></v-divider>
-  </v-app>
+  <v-app>About page</v-app>
 </template>
 <script>
-  export default {
-    name: "About",
-  };
+export default {
+  name: "About",
+};
 </script>
--- cliente/src/views/Archivio.vue
+++ cliente/src/views/Archivio.vue
@@ -1,96 +1,69 @@
 <template v-if="authenticated">
-  <v-app>
-    <h1 class="text-center">Il mio archivio</h1>
-    <v-divider></v-divider>
-    <v-container spacing-playground pa-3>
-      <div>
-        <v-expansion-panels>
-          <v-expansion-panel
-            v-for="(item, index) in archive"
-            :index="index"
-            :key="item._id"
-          >
-            <v-expansion-panel-header>
-              <v-row align="center">
-                <v-col cols="6" sm="3"
-                  ><v-icon>mdi-calendar-range</v-icon>
-                  {{ new Date(item.date).toLocaleDateString("it-IT") }}</v-col
+  <v-container spacing-playground pa-3>
+    <div>
+      <v-expansion-panels>
+        <v-expansion-panel
+          v-for="(item, index) in archive"
+          :index="index"
+          :key="item._id"
+        >
+          <v-expansion-panel-header>
+            <v-row align="center">
+              <v-col cols="6" sm="3"
+                ><v-icon>mdi-calendar-range</v-icon>
+                {{ new Date(item.date).toLocaleDateString("it-IT") }}</v-col
+              >
+              <v-col cols="6" sm="3"
+                ><v-icon>mdi-shoe-print</v-icon> {{ item.meters }}</v-col
+              >
+              <v-col cols="6" sm="3"
+                ><v-icon>mdi-timer</v-icon> {{ item.time }}
+              </v-col>
+              <v-col cols="6" sm="3"
+                ><v-btn
+                  depressed
+                  v-on:click.native.stop="deleteRunAction(item._id)"
+                  color="red white--text"
                 >
-                <v-col cols="6" sm="3"
-                  ><v-icon>mdi-shoe-print</v-icon> {{ item.meters }}</v-col
-                >
-                <v-col cols="6" sm="3"
-                  ><v-icon>mdi-timer</v-icon> {{ item.time }}
-                </v-col>
-                <v-col cols="6" sm="3"
-                  ><v-bottom-sheet v-model="sheet" inset>
-                    <template v-slot:activator="{ on, attrs }">
-                      <v-btn
-                        depressed
-                        v-on:click.native.stop="deleteRunAction(item._id)"
-                        color="red white--text"
-                        dark
-                        v-bind="attrs"
-                        v-on="on"
-                      >
-                        <v-icon>mdi-trash-can-outline</v-icon>
-                      </v-btn>
-                    </template>
-                    <v-sheet class="text-center" height="130px">
-                      <v-btn
-                        class="mt-6"
-                        text
-                        color="error"
-                        @click="sheet = !sheet"
-                        ><v-icon dark left> mdi-close-circle </v-icon>
-                        close
-                      </v-btn>
-                      <div class="my-3">
-                        <v-icon>mdi-checkbox-marked-circle-outline</v-icon>
-                        Corsa eliminata con successo!
-                      </div>
-                    </v-sheet>
-                  </v-bottom-sheet>
-                </v-col>
-              </v-row>
-            </v-expansion-panel-header>
-            <v-expansion-panel-content> v-card </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-expansion-panels>
-      </div>
-    </v-container>
-  </v-app>
+                  <v-icon>mdi-trash-can-outline</v-icon>
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content> v-card </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </div>
+  </v-container>
 </template>
 
 <script>
-  import { mapActions, mapGetters } from "vuex";
-  export default {
-    name: "Archivio",
-    data: () => {
-      return {
-        sheet: false,
-      };
-    },
+import { mapActions, mapGetters } from "vuex";
+export default {
+  name: "Archivio",
+  data: () => {
+    return {};
+  },
 
-    watch: {
-      archive: function() {
-        return this.$store.state.auth.archive
-          ? Object.values(this.$store.state.auth.archive)
-          : "";
-      },
+  watch: {
+    archive: function() {
+      return this.$store.state.auth.archive
+        ? Object.values(this.$store.state.auth.archive)
+        : "";
     },
-    computed: {
-      ...mapGetters({
-        archive: "auth/archive",
-      }),
+  },
+  computed: {
+    ...mapGetters({
+      archive: "auth/archive",
+    }),
+  },
+  methods: {
+    ...mapActions({
+      deleteRun: "auth/deleteRun",
+    }),
+    deleteRunAction(id) {
+      this.deleteRun(id);
     },
-    methods: {
-      ...mapActions({
-        deleteRun: "auth/deleteRun",
-      }),
-      deleteRunAction(id) {
-        this.deleteRun(id);
-      },
-    },
-  };
+  },
+};
 </script>
