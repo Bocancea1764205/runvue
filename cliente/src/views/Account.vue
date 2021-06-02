59d1ed71366e1c8b9b8a446a8915806085d2297e
Bocancea1764205 <bocancea.1764205@studenti.uniroma1.it>
2021-06-02 16:35:28 +0200

    Revert "Revert "Revert "Revert "Merge branch 'main' of https://github.com/Bocancea1764205/runvue into main""""
    This reverts commit 6523b947503bc75b6b3b182ab83b12bb52afc6e6.

--- cliente/src/views/Account.vue
+++ cliente/src/views/Account.vue
@@ -1,21 +1,20 @@
 <template>
   <v-app>
     <div v-if="authenticated">
-      Username account: {{ user.email }}
-      <br />
-      Email account: {{ user.email }}
+      <h1 class="text-center">Il mio account</h1>
     </div>
+    <v-divider></v-divider>
   </v-app>
 </template>
 <script>
-import { mapGetters } from "vuex";
-export default {
-  name: "Account",
-  computed: {
-    ...mapGetters({
-      authenticated: "auth/authenticated",
-      user: "auth/user",
-    }),
-  },
-};
+  import { mapGetters } from "vuex";
+  export default {
+    name: "Account",
+    computed: {
+      ...mapGetters({
+        authenticated: "auth/authenticated",
+        user: "auth/user",
+      }),
+    },
+  };
 </script>
--- cliente/src/views/Run.vue
+++ cliente/src/views/Run.vue
@@ -1,14 +1,10 @@
 <template>
   <v-app>
-    <div v-if="authenticated">
-      Runaton username account: {{ user.username }}
-      <br />
-      Runaton email account: {{ user.email }}
-    </div>
-    <br />
-    <div class="text-center">
-      <strong><Stopwatch /></strong>
-    </div>
+    <h1 class="text-center">Stopwatch</h1>
+    <v-divider></v-divider>
+
+    <h2 class="text-center"><Stopwatch /></h2>
+
     <div class="text-center">
       <v-btn
         v-show="!startedCountdown"
@@ -34,46 +30,45 @@
         ><v-icon dark>mdi-stop</v-icon></v-btn
       >
     </div>
-    <div class="text-center">
-      <strong><Countdown /></strong>
-    </div>
+
+    <h3 class="text-center"><Countdown /></h3>
 
     <br />
     <Tracking />
   </v-app>
 </template>
 <script>
-import store from "../store";
-import Stopwatch from "@/components/Stopwatch";
-import Countdown from "@/components/Countdown";
-import Tracking from "@/components/Tracking";
-import { mapGetters, mapActions } from "vuex";
-import Vue from "vue";
-Vue.use(Tracking);
-export default {
-  store,
-  components: {
-    Stopwatch,
-    Countdown,
-    Tracking,
-  },
-  data: () => ({
-    //
-  }),
-  name: "Run",
-  computed: {
-    ...mapGetters({
-      authenticated: "auth/authenticated",
-      user: "auth/user",
-      startedStopwatch: "start/startedStopwatch",
-      startedCountdown: "start/startedCountdown",
+  import store from "../store";
+  import Stopwatch from "@/components/Stopwatch";
+  import Countdown from "@/components/Countdown";
+  import Tracking from "@/components/Tracking";
+  import { mapGetters, mapActions } from "vuex";
+  import Vue from "vue";
+  Vue.use(Tracking);
+  export default {
+    store,
+    components: {
+      Stopwatch,
+      Countdown,
+      Tracking,
+    },
+    data: () => ({
+      //
     }),
-  },
-  methods: {
-    ...mapActions({
-      startStopwatchHandling: "start/startStopwatchHandling", // map `this.add()` to `this.$store.commit('increment')`
-      startCountdownHandling: "start/startCountdownHandling",
-    }),
-  },
-};
+    name: "Run",
+    computed: {
+      ...mapGetters({
+        authenticated: "auth/authenticated",
+        user: "auth/user",
+        startedStopwatch: "start/startedStopwatch",
+        startedCountdown: "start/startedCountdown",
+      }),
+    },
+    methods: {
+      ...mapActions({
+        startStopwatchHandling: "start/startStopwatchHandling", // map `this.add()` to `this.$store.commit('increment')`
+        startCountdownHandling: "start/startCountdownHandling",
+      }),
+    },
+  };
 </script>
