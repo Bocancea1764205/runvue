1092fb7d2a89ba79750dbad9af25b8fd38c6d550
Bocancea1764205 <bocancea.1764205@studenti.uniroma1.it>
2021-06-02 16:38:22 +0200

    Revert "Revert "Revert "Revert "Merge branch 'main' of https://github.com/Bocancea1764205/runvue into main""""
    This reverts commit 6523b947503bc75b6b3b182ab83b12bb52afc6e6.

--- cliente/src/views/Signup.vue
+++ cliente/src/views/Signup.vue
@@ -1,120 +1,124 @@
 <template>
-  <v-container>
-    <v-form ref="form" v-model="valid" lazy-validation>
-      <v-text-field
-        type="text"
-        v-model="form.username"
-        :counter="10"
-        :rules="nameRules"
-        label="Username"
-        required
-      ></v-text-field>
+  <v-app>
+    <h1 class="text-center">Signup</h1>
+    <v-divider></v-divider>
+    <v-container>
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-text-field
+          type="text"
+          v-model="form.username"
+          :counter="10"
+          :rules="nameRules"
+          label="Username"
+          required
+        ></v-text-field>
 
-      <v-text-field
-        type="email"
-        v-model="form.email"
-        :rules="emailRules"
-        label="E-mail"
-        required
-      ></v-text-field>
+        <v-text-field
+          type="email"
+          v-model="form.email"
+          :rules="emailRules"
+          label="E-mail"
+          required
+        ></v-text-field>
 
-      <v-text-field
-        type="password"
-        v-model="form.password"
-        :counter="15"
-        :rules="pwRules"
-        label="Password"
-        required
-      ></v-text-field>
+        <v-text-field
+          type="password"
+          v-model="form.password"
+          :counter="15"
+          :rules="pwRules"
+          label="Password"
+          required
+        ></v-text-field>
 
-      <v-checkbox
-        v-model="checkbox"
-        :rules="[(v) => !!v || 'Devi accettare per continuare!']"
-        label="Do you agree?"
-        required
-      ></v-checkbox>
+        <v-checkbox
+          v-model="checkbox"
+          :rules="[(v) => !!v || 'Devi accettare per continuare!']"
+          label="Do you agree?"
+          required
+        ></v-checkbox>
 
-      <v-btn
-        :disabled="!valid"
-        color="success"
-        class="mr-4"
-        @click="
-          submit;
-          validate;
-        "
-        ><v-icon dark left> mdi-account-plus-outline </v-icon>Signup</v-btn
-      >
-      <v-btn color="error" class="mr-4" @click="reset"
-        ><v-icon dark left> mdi-close-box-outline </v-icon>Reset Signup</v-btn
-      >
-      <br />
-      <br />
-      <router-link to="/forgotPassword">Password dimenticata?</router-link>
-      <br />
-      <router-link to="/login">Hai già un account? Fai il login!</router-link>
-      {{ error }}
-    </v-form>
-  </v-container>
+        <v-btn
+          :disabled="!valid"
+          color="success"
+          class="mr-4"
+          @click="
+            submit;
+            validate;
+          "
+          ><v-icon dark left> mdi-account-plus-outline </v-icon>Signup</v-btn
+        >
+        <v-btn color="error" class="mr-4" @click="reset"
+          ><v-icon dark left> mdi-close-box-outline </v-icon>Reset Signup</v-btn
+        >
+        <br />
+        <br />
+        <router-link to="/forgotPassword">Password dimenticata?</router-link>
+        <br />
+        <router-link to="/login">Hai già un account? Fai il login!</router-link>
+        {{ error }}
+      </v-form>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import { mapActions, mapGetters } from "vuex";
-export default {
-  name: "Signup",
-  data: () => {
-    return {
-      valid: true,
-      nameRules: [
-        (v) => !!v || "Username obbligatorio",
-        (v) =>
-          (v && v.length <= 10) ||
-          "Username deve contenere meno di 5 caratteri",
-      ],
-      emailRules: [
-        (v) => !!v || "E-mail obbligatoria",
-        (v) => /.+@.+\..+/.test(v) || "E-mail deve essere valida",
-      ],
-      pwRules: [
-        (v) => !!v || "Password obbligatoria",
-        (v) =>
-          (v && v.length <= 15) ||
-          "Password deve contenere meno di 15 caratteri",
-        (v) => /(?=.*[A-Z])/.test(v) || "Password deve avere una maiuscola",
-        (v) => /(?=.*\d)/.test(v) || "Password deve avere un numero",
-        (v) =>
-          /([!@$%])/.test(v) ||
-          "Password deve avere un carattere speciale [!@#$%]",
-      ],
-      select: null,
-      checkbox: false,
-      form: {
-        username: "",
-        email: "",
-        password: "",
+  import { mapActions, mapGetters } from "vuex";
+  export default {
+    name: "Signup",
+    data: () => {
+      return {
+        valid: true,
+        nameRules: [
+          (v) => !!v || "Username obbligatorio",
+          (v) =>
+            (v && v.length <= 10) ||
+            "Username deve contenere meno di 5 caratteri",
+        ],
+        emailRules: [
+          (v) => !!v || "E-mail obbligatoria",
+          (v) => /.+@.+\..+/.test(v) || "E-mail deve essere valida",
+        ],
+        pwRules: [
+          (v) => !!v || "Password obbligatoria",
+          (v) =>
+            (v && v.length <= 15) ||
+            "Password deve contenere meno di 15 caratteri",
+          (v) => /(?=.*[A-Z])/.test(v) || "Password deve avere una maiuscola",
+          (v) => /(?=.*\d)/.test(v) || "Password deve avere un numero",
+          (v) =>
+            /([!@$%])/.test(v) ||
+            "Password deve avere un carattere speciale [!@#$%]",
+        ],
+        select: null,
+        checkbox: false,
+        form: {
+          username: "",
+          email: "",
+          password: "",
+        },
+        error: "",
+      };
+    },
+    computed: {
+      ...mapGetters({
+        authenticated: "auth/authenticated",
+      }),
+    },
+    methods: {
+      ...mapActions({
+        signUp: "auth/signUp",
+      }),
+      submit() {
+        this.logIn(this.form).then(() => {
+          if (this.authenticated) this.$router.replace({ name: "Run" });
+        });
       },
-      error: "",
-    };
-  },
-  computed: {
-    ...mapGetters({
-      authenticated: "auth/authenticated",
-    }),
-  },
-  methods: {
-    ...mapActions({
-      signUp: "auth/signUp",
-    }),
-    submit() {
-      this.logIn(this.form).then(() => {
-        if (this.authenticated) this.$router.replace({ name: "Run" });
-      });
+      validate() {
+        this.$refs.form.validate();
+      },
+      reset() {
+        this.$refs.form.reset();
+      },
     },
-    validate() {
-      this.$refs.form.validate();
-    },
-    reset() {
-      this.$refs.form.reset();
-    },
-  },
-};
+  };
 </script>
