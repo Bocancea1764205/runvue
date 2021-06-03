97a9f1bf61a1b5fd52211a7febb120969c1b34b0
alessiobuccarella <alexbucc99@gmail.com>
2021-06-03 18:06:59 +0200

    ultime modifiche

--- cliente/src/views/Run.vue
+++ cliente/src/views/Run.vue
@@ -1,79 +1,86 @@
 <template>
   <v-app>
-    <div v-if="authenticated">
-      Runaton username account: {{ user.username }}
+    <v-container>
+      <h1 class="text-center">Run</h1>
+      <v-divider></v-divider>
       <br />
-      Runaton email account: {{ user.email }}
-    </div>
-    <br />
-    <div class="text-center">
-      <strong><Stopwatch /></strong>
-    </div>
-    <div class="text-center">
-      <v-btn
-        v-show="!startedCountdown"
-        color="success"
-        fab
-        elevation="11"
-        x-large
-        @click="startCountdownHandling()"
-        ><v-icon dark>mdi-play</v-icon></v-btn
-      >
-      <v-btn
-        v-show="startedCountdown"
-        color="error"
-        fab
-        elevation="11"
-        x-large
-        @click="
-          if (startedCountdown) {
-            startCountdownHandling();
-            if (startedStopwatch) startStopwatchHandling();
-          }
-        "
-        ><v-icon dark>mdi-stop</v-icon></v-btn
-      >
-    </div>
-    <div class="text-center">
-      <strong><Countdown /></strong>
-    </div>
+      <div class="text-center">
+        <h1>
+          <strong><Stopwatch /></strong>
+        </h1>
+      </div>
+      <br />
+      <div class="text-center">
+        <v-btn
+          height="100px"
+          width="100px"
+          v-show="!startedCountdown"
+          color="success"
+          elevation="11"
+          x-large
+          @click="startCountdownHandling()"
+          ><v-icon dark size="60px">mdi-play</v-icon></v-btn
+        >
+        <v-btn
+          height="100px"
+          width="100px"
+          v-show="startedCountdown"
+          color="error"
+          elevation="11"
+          x-large
+          @click="
+            if (startedCountdown) {
+              startCountdownHandling();
+              if (startedStopwatch) startStopwatchHandling();
+            }
+          "
+          ><v-icon dark size="60px">mdi-stop</v-icon></v-btn
+        >
+      </div>
+      <br />
+      <div class="text-center">
+        <h2>
+          <Countdown />
+        </h2>
+      </div>
 
-    <br />
-    <Tracking />
+      <br />
+      <Tracking />
+    </v-container>
   </v-app>
 </template>
 <script>
-import store from "../store";
-import Stopwatch from "@/components/Stopwatch";
-import Countdown from "@/components/Countdown";
-import Tracking from "@/components/Tracking";
-import { mapGetters, mapActions } from "vuex";
-import Vue from "vue";
-Vue.use(Tracking);
-export default {
-  store,
-  components: {
-    Stopwatch,
-    Countdown,
-    Tracking,
-  },
-  data: () => ({
-    //
-  }),
-  name: "Run",
-  computed: {
-    ...mapGetters({
-      authenticated: "auth/authenticated",
-      user: "auth/user",
-      startedStopwatch: "start/startedStopwatch",
-      startedCountdown: "start/startedCountdown",
+  import store from "../store";
+  import Stopwatch from "@/components/Stopwatch";
+  import Countdown from "@/components/Countdown";
+  import Tracking from "@/components/Tracking";
+  import { mapGetters, mapActions } from "vuex";
+  import Vue from "vue";
+  Vue.use(Tracking);
+  export default {
+    store,
+    components: {
+      Stopwatch,
+      Countdown,
+      Tracking,
+    },
+    data: () => ({
+      //
     }),
-  },
-  methods: {
-    ...mapActions({
-      startStopwatchHandling: "start/startStopwatchHandling", // map `this.add()` to `this.$store.commit('increment')`
-      startCountdownHandling: "start/startCountdownHandling",
-    }),
-  },
-};
+    name: "Run",
+    computed: {
+      ...mapGetters({
+        authenticated: "auth/authenticated",
+        user: "auth/user",
+        startedStopwatch: "start/startedStopwatch",
+        startedCountdown: "start/startedCountdown",
+      }),
+    },
+    methods: {
+      ...mapActions({
+        startStopwatchHandling: "start/startStopwatchHandling", // map `this.add()` to `this.$store.commit('increment')`
+        startCountdownHandling: "start/startCountdownHandling",
+      }),
+    },
+  };
 </script>
